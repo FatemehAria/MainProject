2e1476660ca8af176ef3e60730ef0fe572277b3f
FatemehAria <ftemewh@gmail.com>
2024-08-29 18:16:24 +0330

    Database connection separation initialized

---
+++ Repositories/DatabaseConnection.cs
@@ -0,0 +1,23 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IDatabaseConnection
+    {
+        Task<CustomActionResult<IDbConnection>> connectToDatabase();
+    }
+    public class DatabaseConnection : IDatabaseConnection
+    {
+        private IDbConnection _connection;
+        public Task<CustomActionResult<IDbConnection>> connectToDatabase()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
