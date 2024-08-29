0b2878db2abc864d67dcc7d0ef4add545a46a59c
FatemehAria <ftemewh@gmail.com>
2024-08-29 23:41:02 +0330

    userlogin service logic added

--- Repositories/UserLoginRepository.cs
+++ Repositories/UserLoginRepository.cs
@@ -1,22 +1,46 @@
-﻿using Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dapper;
+using Models;
 
 namespace Repositories
 {
     public interface IUserLoginRepository
     {
-        Task<CustomActionResult<decimal>> getUserByUsernameAndPassword(string username, string password);
+        Task<CustomActionResult<decimal>> getUserByUsernameAndPassword(UserModel model);
     }
 
     public class UserLoginRepository : IUserLoginRepository
     {
-        public Task<CustomActionResult<decimal>> getUserByUsernameAndPassword(string username, string password)
+        private readonly IDatabaseConnection _dbConnection;
+
+        public UserLoginRepository(IDatabaseConnection dbConnection)
         {
-            throw new NotImplementedException();
+            _dbConnection = dbConnection;
+        }
+        public async Task<CustomActionResult<decimal>> getUserByUsernameAndPassword(UserModel model)
+        {
+
+            CustomActionResult<decimal> result = new CustomActionResult<decimal>();
+            try
+            {
+                var connection = await _dbConnection.connectToDatabase();
+                if (!connection.success) return result;
+
+                var command = "prc_login_user";
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add(name: "username", value: model.phoneNumber);
+                parameters.Add(name: "password", value: model.password);
+
+                await connection.data.ExecuteAsync(command, parameters, commandType: System.Data.CommandType.StoredProcedure);
+                result.message = "login successful.";
+                result.success = true;
+
+            }
+            catch
+            {
+                result.message = "login failed.";
+                result.success = false;
+            }
+            return result;
         }
     }
 }
