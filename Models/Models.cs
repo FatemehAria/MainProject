2a746d3034db8f78afd0546d18a74884aaf389a9
FatemehAria <ftemewh@gmail.com>
2024-08-29 12:59:35 +0330

    user model initialized

---
+++ Models/Models.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class UserModel
+    {
+        public int userId { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string phoneNumber { get; set; }
+    }
+}
