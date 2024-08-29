6de99bbb72c524a16edc053551da6f674a9ab1bf
FatemehAria <ftemewh@gmail.com>
2024-08-29 16:39:24 +0330

    custom action result for returning to front created

---
+++ Models/CustomActionResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class CustomActionResult
+    {
+        public string message { get; set; }
+        public bool success { get; set; }
+
+    }
+
+    public class CustomActionResult<T> : CustomActionResult
+    {
+        public T data { get; set; }
+
+    }
+}
