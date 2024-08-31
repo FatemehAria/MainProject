8e2cc9d5da805b29acb8d0d5f04174d2d26fda1a
FatemehAria <ftemewh@gmail.com>
2024-08-31 16:09:26 +0330

    toasts added

--- front-codes/src/components/Users.tsx
+++ front-codes/src/components/Users.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import app from "../service/service";
 import EditUserForm from "./EditUserForm";
 import { getUsers } from "../utils/util";
+import toast from "react-hot-toast";
 
 function Users() {
   const [allUsers, setAllUsers] = useState([]);
@@ -15,15 +16,18 @@ function Users() {
       const { data } = await app.post("/User/DeleteUserById", {
         userId,
       });
+      toast.success("کاربر با موفقیت حذف شد.");
       console.log(data);
     } catch (error) {
+      toast.error("خطا در حذف کاربر.");
       console.log(error);
     }
   };
-  
+
   useEffect(() => {
     getUsers(setAllUsers);
-  }, []);
+  }, [showEditForm.show]);
+
   return (
     <div className="text-center">
       <div
@@ -59,7 +63,11 @@ function Users() {
           ) => (
             <div
               key={item.userId}
-              className="grid grid-cols-5 justify-center items-center"
+              className={`grid grid-cols-5 justify-center items-center ${
+                item.userId === +showEditForm.userId
+                  ? "bg-yellow-400 rounded-lg py-2 text-white text-lg"
+                  : ""
+              }`}
             >
               <p className="font-semibold">{index + 1}</p>
               <p>{item.firstName}</p>
